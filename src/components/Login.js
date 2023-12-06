b2e43315f6a1d7969d7c67294a95eb6f96909a44
Shivam Mishra <shivammishra@Shivams-MacBook-Air-2.local>
2023-12-05 21:10:44 -0500

    Adding update operation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 import Home from './Home';
 import { Routes, Route, Switch, BrowserRouter, useNavigate } from 'react-router-dom';
 import './login.css'
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
+import { format, parseISO } from 'date-fns';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -17,6 +20,7 @@ const Login = () => {
   const [cfirstname, SetCFirstname] = useState('');
   const [items, setItems] = useState("");
 
+  const chartRef = useRef(null);
 
 // API Function
 
@@ -64,6 +68,15 @@ const Login = () => {
     }
   };
 
+  const [load, SetLoad] = useState(false);
+
+  function setload(){
+    setload(!load);
+  }
+
+  // *******************************  Load Data *****************************************
+  const [displaydates, SetDisplayDates] = useState('');
+  const [displayamount, SetDisplayAmount] = useState('');
   // load data
   const loaddata = async (e) => {
     e.preventDefault();
@@ -78,11 +91,19 @@ const Login = () => {
 
       const data = await response.json();
       setItems(data);
+
+      const dates = data.items.map(item => new Date(item.date));
+      const amounts = data.items.map(item => item.amount);
+
+      SetDisplayAmount(amounts);
+      SetDisplayDates(dates);
+
+      setload();
     
     } catch (error) {
       console.error('Error:', error);
     }
-    };
+  };
     
     function change_stat(){
       setStat(true)
@@ -122,6 +143,38 @@ const Login = () => {
         stat_delete_false();
         };
 
+    // Update
+
+    const [updatestate, setUpdatestate] = useState(false)
+    const [uid, setuiId] = useState('')
+    const [udetails, setUdetails] = useState('')
+    const [uamount, setUamount] = useState('')
+
+
+      function ustatus(){
+        setUpdatestate(!updatestate);
+      };
+
+
+    const updateData = async (e) => {
+      e.preventDefault();
+      try { 
+        const response = await fetch('http://127.0.0.1:5000/update', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({uid, uamount, udetails}),
+        });
+  
+        const data = await response.json();
+      
+      } catch (error) {
+        console.error('Error:', error);
+      }
+      ustatus();
+      };
+
     // Add items
     const addData = async (e) => {
       e.preventDefault();
@@ -150,6 +203,7 @@ const Login = () => {
 
 
   const add_data_form = (
+    <div className='add_data'>
     <form onSubmit={addData}>
         <label>Email: {email}</label>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
@@ -165,51 +219,69 @@ const Login = () => {
       
         <button type="submit">Submit</button>
       </form>
+      </div>
   );
 
   const del_data_form = (
+    <div className='del_data'>
     <form onSubmit={deleteData}>
-        <label>id: {delid}</label>
+        <label>Enter the id to be deleted:</label>
         <input type="id" value={delid} onChange={(e) => setDelId(e.target.value)} required />
         <button type="submit">Submit</button>
       </form>
+      </div>
     );
 
+    const update_data_form = (
+      <div className='update_data'>
+      <form onSubmit={updateData}>
+          <label>id:</label>
+          <input type="id" value={uid} onChange={(e) => setuiId(e.target.value)} required />
+          <label>detail:</label>
+          <input type="detail" value={udetails} onChange={(e) => setUdetails(e.target.value)} required />
+          <label>amount:</label>
+          <input type="amount" value={uamount} onChange={(e) => setUamount(e.target.value)} required />
+          <button type="submit">Submit</button>
+        </form>
+        </div>
+      );
+
+// ********************************* Chart Items ******************************
 
 // ********************************* Login Signup ******************************
-  const form = (
-    <div>
-    <div>
-      <p>Login:</p>
-      <form onSubmit={handleLogin}>
-        <label>Email:</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+  const lform = (
+    <div className='basic'>
+      <div className='login'>
+        <h2>Login:</h2>
+        <form onSubmit={handleLogin}>
+          <label>Email:</label>
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      </div>
+    )
 
-        <label>Password:</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-      
-        <button type="submit">Submit</button>
-      </form>
-      
-    </div>
-    <div>
-      <p>Signup:</p>
-    <form onSubmit={handleAddUser}>
-        <label>First Name:</label>
-        <input type="text" value={addfirstname} onChange={(e) => setAddFirstName(e.target.value)} required />
-        <br/>
-        <label>Last Name:</label>
-        <input type="text" value={addlastname} onChange={(e) => setAddLastName(e.target.value)} required />
-        <br/>
-        <label>Email:</label>
-        <input type="email" value={addemail} onChange={(e) => setAddEmail(e.target.value)} required />
-        <br/>
-        <label>Password:</label>
-        <input type="password" value={addpassword} onChange={(e) => setAddPassword(e.target.value)} required />
-        <br/>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    const sform = (
+      <div>
+      <div className='sign'>
+        <h2>Sign up:</h2>
+        <form onSubmit={handleAddUser}>
+          <label>First Name:</label>
+          <input type="text" value={addfirstname} onChange={(e) => setAddFirstName(e.target.value)} required />
+          <label>Last Name:</label>
+          <input type="text" value={addlastname} onChange={(e) => setAddLastName(e.target.value)} required />
+          <br/>
+          <label>Email:</label>
+          <input type="email" value={addemail} onChange={(e) => setAddEmail(e.target.value)} required />
+          <label>Password:</label>
+          <input type="password" value={addpassword} onChange={(e) => setAddPassword(e.target.value)} required />
+          <br/>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 
@@ -227,39 +299,35 @@ const Login = () => {
   const home = (
     
       <div>
-      <div>Home</div>
-  
-      <div>Welcome : {email}</div>
-      <div>{stat ? add_data_form : <button onClick={change_stat}>Add</button>} </div>
-      <div>{delState ? del_data_form : <button onClick={stat_delete}>Delete</button>} </div>
-      <div>Items : <button onClick={loaddata}>Load Items</button> </div>
+      <div className='intro'><h2>Account : {email}</h2></div>
+      <div className='add_data'>{stat ? add_data_form : <button onClick={change_stat}>Add</button>} </div>
+      <div className='deletedata'>{delState ? del_data_form : <button onClick={stat_delete}>Delete</button>} </div>
+      <div className='update'>{updatestate?update_data_form:<button onClick={ustatus}>Update</button>}</div>
+      <div className='list'><button onClick={loaddata}>Load Items</button></div>
       {items.items && items.items.map(items => {
             return(
+              <div className='form_box'>
                 <div key={items.id} className='row'>
                     <h5>email: {items.email} </h5>
                     <p>id: {items.id}</p> 
-                    <p> date: {items.date}</p>
-                    <p>amount: {items.amount}</p>
+                    <p>date: {items.date}</p>
+                    <p><mark>amount: {items.amount}</mark></p>
                     <p>details: {items.details}</p>
-                    <div className='row'>
-                       <div className='col-md-1'> <button className='btn btn-primary' onClick={() => updateItems(items)}>Update</button></div>
-                       <div className='col'><button className='btn btn-danger'
-                        onClick={() => deleteItems(items)}>Delete</button></div>
-                    </div>
+                </div>
                 </div>
             )
         })
         }
       </div>
-    
   );
 
 
 // ******************************** Function Return **************************
   return (
     <div className="app">
+      <header><h2>Welcome to Expense Tracker</h2></header>
     <div className="login-form">
-      {response=="True" ? <div>{home}</div>: <div>{form}</div>}
+      {response==="True" ? <div>{home}</div>:<div className='bck'>{lform}{sform}</div>}
     </div>
   </div>
     
